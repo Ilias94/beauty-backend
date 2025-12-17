70ef86127117c3f414fd0311f693173c32b8fec2
Ilias Belghoul <ilias.belghoul@gmail.com>
2025-12-17 20:21:13 +0100

    fix UserMapperTest

--- src/test/java/pl/ib/beauty/mapper/UserMapperTest.java
+++ src/test/java/pl/ib/beauty/mapper/UserMapperTest.java
@@ -23,7 +23,7 @@ class UserMapperTest {
     void shouldMapUserToDto() {
         //given
         Course course = new Course(1L, "hair cut", "man hair cut course", LocalDateTime.now(), LocalDateTime.now(), 10, 4.0, BigDecimal.TEN, null, null, null, null);
-        User user = new User(1L, "Adam", "Nowak", "adam@gmail.com", "adam123", Set.of(new Role(1L, "user")), List.of(course), null,null,null);
+        User user = new User(1L, "Adam", "Nowak", "adam@gmail.com", "adam123", Set.of(new Role(1L, "user")), List.of(course), null, null, null);
 
         //when
         UserDtoResponse result = userMapper.userToDto(user);
@@ -31,14 +31,10 @@ class UserMapperTest {
         //then
         assertNotNull(result);
         assertEquals(1L, user.getId());
-        assertEquals("Adam", result.firstName());
-        assertEquals("Nowak", result.lastName());
-        assertEquals("Nowak", result.lastName());
-        assertEquals("adam@gmail.com", result.email());
-        assertNull(result.password());
-        assertNull(result.confirmPassword());
-        assertNotNull(result.revisionNumber());
-        assertNotNull(List.of(new Role(1L, "user"), result.roles()));
+        assertEquals("Adam", result.getFirstName());
+        assertEquals("Nowak", result.getLastName());
+        assertEquals("adam@gmail.com", result.getEmail());
+        assertNotNull(List.of(new Role(1L, "user"), result.getRoles()));
         assertFalse(result.isTeacher());
     }
 
